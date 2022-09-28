8d8b884331173e8c38e455f6046b0f972fb745e1
Emma Amalie Pedersen <eapeders@stud.ntnu.no>
2022-09-28 21:58:29 +0200

    #11 Implementet CommitFetcher, changed name and logo

---
+++ meshlab/src/components/CommitFetcher.tsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+
+// glpat-VVibRbJ7pSfHKcYLnU5S   gitlab AC OLD NOT WORKING
+// glpat-Fy8Cs4SqsPRrBa6MirZy new one with role = developer
+
+type CData = {
+  author_name: string,
+  committed_date: string,
+  id: string,
+  message: string,
+}
+
+function CommitFetcher() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [data, setData] = useState<CData[]>([]);
+
+    useEffect(() => {
+
+        fetch('https://gitlab.stud.idi.ntnu.no/api/v4/projects/17628/repository/commits'
+            , {
+                headers: {
+                    "PRIVATE-TOKEN": "glpat-Fy8Cs4SqsPRrBa6MirZy", // our projects access token
+                    'Content-Type': 'application/json',
+                    'Accept': 'appliaction/json'
+                }
+            }
+        )
+            .then(function (res): Response {
+                if (!res.ok) {
+                    throw Error(res.statusText)
+                }
+                return res
+            })
+            .then(res => res.json())
+            .then(
+                (result: Promise<any>) => {
+                    setIsLoaded(true);
+                    const response = result as unknown as CData[];
+                    console.log(response);
+                    setData(response);
+                }
+            )
+            .catch(
+                (err) => {
+                    setIsLoaded(true);
+                    console.log(err);
+                    setError(err);
+                }
+            )
+
+    }, [])
+
+    //return JSX: if there was an error: tell the user, otherwise return the data
+    if (error) {
+        return <p> Something went wrong with fetching the data. Are you sure there are no spelling mistakes in your url, and you have the correct accesses? (make sure you're using the correct access token)</p>
+    } else if (!isLoaded) {
+        return <p>Loading...</p>
+
+  } else {
+    return (
+      <Box sx={{ height: 400, width: '90%', margin: "0 auto 0 auto" }}>
+      <DataGrid
+        rows={data.map((commit: CData) => (
+          { author_name: commit.author_name, committed_date: commit.committed_date.substring(0,10), id: commit.id, message: commit.message}
+          ))}
+        columns={[
+        {
+          field: 'committed_date',
+          headerName: 'Date',
+          width: 100,
+        },
+        {
+          field: 'message',
+          headerName: 'Message',
+          width: 650,
+        },
+        {
+          field: 'author_name',
+          headerName: 'Author',
+          width: 300,
+        },
+        {
+          field: 'id',
+          headerName: 'ID',
+          width: 200
+        },]}
+        getRowId={(row) => row.id}
+        pageSize={data.length}
+        rowsPerPageOptions={[data.length]}
+        experimentalFeatures={{ newEditingApi: true }}
+      />
+    </Box>
+    );
+  }
+  }
+
+export { CommitFetcher }
--- meshlab/src/components/UserFilter.tsx
+++ meshlab/src/components/UserFilter.tsx
@@ -7,6 +7,7 @@ import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 import './UserFilter.css'
 
+//Delete this file
 export default function UserFilter() {
   const [user, setUser] = React.useState('');
 
