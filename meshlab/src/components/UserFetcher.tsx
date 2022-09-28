d94b16a49d7b28b1d4aa4413cda913f17cb0e704
Hang Celin Le <hcle@stud.ntnu.no>
2022-09-28 12:06:58 +0200

    #6 display user data in grid

--- meshlab/src/components/UserFetcher.tsx
+++ meshlab/src/components/UserFetcher.tsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid, GridColDef, selectedGridRowsCountSelector } from '@mui/x-data-grid';
+import userEvent from '@testing-library/user-event';
 
 // glpat-VVibRbJ7pSfHKcYLnU5S   gitlab AC OLD NOT WORKING
 // glpat-Fy8Cs4SqsPRrBa6MirZy new one with role = developer
@@ -13,13 +16,11 @@ type UData = {
 }
 
 function UserFetcher() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [data, setData] = useState<UData[]>([]);
-    useEffect(() => {
 
-        fetch('https://gitlab.stud.idi.ntnu.no/api/v4/projects/17628/members/all'
-            //'https://gitlab.stud.idi.ntnu.no/api/v4/projects/17628/members/all/{id} to get one of the members'
+    const [data, setData] = useState<UData[]>([]);
+    const getData = () => {
+        fetch('https://gitlab.stud.idi.ntnu.no/api/v4/projects/17628/users'
+        //'https://gitlab.stud.idi.ntnu.no/api/v4/projects/17628/members/all/{id} to get one of the members'
             , {
                 headers: {
                     "PRIVATE-TOKEN": "glpat-Fy8Cs4SqsPRrBa6MirZy", // our projects access token
@@ -28,47 +29,44 @@ function UserFetcher() {
                 }
             }
         )
-            .then(function (res): Response {
-                if (!res.ok) {
-                    throw Error(res.statusText)
-                }
-                return res
+            .then(function (response) {
+                return response.json() as unknown as UData[];
             })
-            .then(res => res.json())
-            .then(
-                (result: Promise<any>) => {
-                    setIsLoaded(true);
-                    const response = result as unknown as UData[];
-                    setData(response);
-                }
-            )
-            .catch(
-                (err) => {
-                    setIsLoaded(true);
-                    console.log(err);
-                    setError(err);
-                }
-            )
-
+            .then(function (myJson) {
+                console.log(myJson);
+                setData(myJson);
+            });
+    }
+    useEffect(() => {
+        getData()
     }, [])
 
-    //return JSX: if there was an error: tell the user, otherwise return the data
-    if (error) {
-        return <p> Something went wrong with fetching the data. Are you sure there are no spelling mistakes in your url, and you have the correct accesses? (make sure you're using the correct access token)</p>
-    } else if (!isLoaded) {
-        return <p>Loading...</p>
-
-    } else {
-        return <div>
-            <p>Users:</p>
-            {
-                <ul>
-                    {data && data.length > 0 && data.map((users) => <li key={users.id}>{users.username} ({users.name})</li>)}
-                </ul>
-            }
-        </div >
-    }
-
+  return (
+    <Box sx={{ height: 400, width: '100%'}}>
+    <DataGrid
+      rows={data.map((user: UData) => (
+        { id: user.id, username: user.username, fullName: user.name}
+        ))}
+      columns={[{ field: 'id', headerName: 'ID', width: 90 },
+      {
+        field: 'username',
+        headerName: 'Username',
+        width: 150,
+      },
+      {
+        field: 'fullName',
+        headerName: 'Full name',
+        width: 150,
+      },]}
+      getRowId={(row) => row.id}
+      pageSize={data.length}
+      rowsPerPageOptions={[data.length]}
+      checkboxSelection
+      disableSelectionOnClick
+      experimentalFeatures={{ newEditingApi: true }}
+    />
+  </Box>
+  );
 }
 
 export { UserFetcher }
