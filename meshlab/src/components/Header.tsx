5c7dd32a5869dfbdda14c253d233e2d34f3db372
Emma Amalie Pedersen <eapeders@stud.ntnu.no>
2022-09-30 11:09:52 +0200

    Turned the Header-file into a class based component

--- meshlab/src/components/Header.tsx
+++ meshlab/src/components/Header.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Header.css'
 
-function Header() {
-  return (
-    <div id="HeaderContainer">
-      <h1>MeshLab</h1>
-    </div>
-  )
+class Header extends React.Component {
+  render() {
+    return (
+      <div id="HeaderContainer">
+        <h1>MeshLab</h1>
+      </div>
+    )
+  }
 }
 
 export default Header
