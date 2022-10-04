07e3490a8b9a1bf702eb95355080bf80b5c9189c
Emma Amalie Pedersen <eapeders@stud.ntnu.no>
2022-10-04 08:45:27 +0200

    #23 Started implementing testing

--- meshlab/src/App.test.tsx
+++ meshlab/src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { createRenderer } from 'react-dom/test-utils';
+import renderer from 'react-test-renderer';
+import Button from './components/Button';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Button
+      onClick={function (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+        throw new Error('Function not implemented.');
+      } }
+      ></Button>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+})
